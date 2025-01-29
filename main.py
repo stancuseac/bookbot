c29ff58e699f02f0baf03d6e59fd61f06eb48e5a
superstan <stancuseac@gmail.com>
2025-01-29 10:16:03 -0800

    updated formatting

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} words in this document")
-    print(count_characters(text))
+    char_dict = count_characters(text)
+    #print(f"{num_words} words in this document")
+    #print(char_dict)
+    sorted_letters(char_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,19 +14,6 @@ def get_book_text(path):
 def count_words(text):
     split_text = text.split()
     return len(split_text)
-
-'''
-def count_unique_words(text):
-    split_text = text.lower().split()
-    unique_counts = {}
-    for word in split_text:
-        if word in unique_counts:
-            unique_counts[word] += 1
-        else: 
-            unique_counts[word] = 1
-    print(unique_counts)
-    # find out how to remove punctiation and sort the dictionary A-Z
-'''
 
 def count_characters(text):
     char_dict = {}
@@ -35,6 +24,14 @@ def count_characters(text):
             else: 
                 char_dict[char] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_letters(dict):
+    listed_dict = [num for x in dict]
+    listed_dict.sort(reverse=False, key=sort_on)
+    print(dict)
 
 if __name__=="__main__":
     main()
